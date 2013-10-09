ad992581c28e7c3ba374ba130d4f76883de69fdd
Alan <username@franzoni.eu>
2013-10-09 09:02:21 +0200

    Site updated at 2013-10-09 07:02:21 UTC

--- downloads/code/quicklog.sh
+++ downloads/code/quicklog.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# set LOGFILE to the full path of your desired logfile; make sure
+# you have write permissions there. set RETAIN_NUM_LINES to the
+# maximum number of lines that should be retained at the beginning
+# of your program execution.
+# execute 'logsetup' once at the beginning of your script, then 
+# use 'log' how many times you like.
+
 LOGFILE=quicklog.sh.log
 RETAIN_NUM_LINES=10
 
